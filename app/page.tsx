ffd240f1ff125e6a119dc550ac215b99ffdcd5a6
josealejandre <JOSE.ALEJANDRE1996@gmail.com>
2026-06-04 16:50:28 +0200

    fix: page.tsx con NichoView

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Navigation from "@/components/Navigation";
 import ModuleView from "@/components/ModuleView";
 import ProblemasView from "@/components/ProblemasView";
 import SolucionesView from "@/components/SolucionesView";
+import NichoView from "@/components/NichoView";
 import { modules } from "@/lib/modules";
 
 export default function Home() {
@@ -13,22 +14,13 @@ export default function Home() {
 
   return (
     <div style={{ minHeight: "100vh", backgroundColor: "#0f0f13" }}>
-      <Navigation
-        modules={modules}
-        activeModule={activeModuleId}
-        onModuleChange={setActiveModuleId}
-      />
+      <Navigation modules={modules} activeModule={activeModuleId} onModuleChange={setActiveModuleId} />
       {currentModule.id === "problemas" ? (
-        <ProblemasView
-          module={currentModule}
-          seleccionados={problemasSeleccionados}
-          onSeleccionChange={setProblemasSeleccionados}
-        />
+        <ProblemasView module={currentModule} seleccionados={problemasSeleccionados} onSeleccionChange={setProblemasSeleccionados} />
       ) : currentModule.id === "soluciones" ? (
-        <SolucionesView
-          module={currentModule}
-          problemasSeleccionados={problemasSeleccionados}
-        />
+        <SolucionesView module={currentModule} problemasSeleccionados={problemasSeleccionados} />
+      ) : currentModule.id === "nicho" ? (
+        <NichoView module={currentModule} />
       ) : (
         <ModuleView module={currentModule} />
       )}
